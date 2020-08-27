7cd6938eda03290bbce364ef45fc899573b7cf71
Paul Lynch <lynchpw@grizzlies.adams.adams.edu>
2020-08-27 16:50:23 -0600

    more stuff version 1

--- helloCSCI206.java
+++ helloCSCI206.java
@@ -8,5 +8,7 @@ public class helloCSCI206 {
     public static void main(String[] args) {
 
         System.out.println("Hello CSCI 206 World");
+        System.out.println("Hello CSCI 206 World");
+        System.out.println("Hello CSCI 206 World");
     }
 }
